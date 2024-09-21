13d48cc93d4513325407efda68da3d95a259e5cd
manthan3105 <36650711+manthan3105@users.noreply.github.com>
2024-09-21 22:11:38 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/vmac/WatBot/onboarding/DefaultIntro.java
@@ -0,0 +1,67 @@
+package com.example.vmac.WatBot.onboarding;
+
+import android.app.ActionBar;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.github.paolorotolo.appintro.AppIntro;
+import com.github.paolorotolo.appintro.AppIntroFragment;
+import com.example.vmac.WatBot.R;
+
+/**
+ * Created by Amal Krishnan on 25-05-2017.
+ */
+
+public class DefaultIntro extends AppIntro {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+
+        addSlide(AppIntroFragment.newInstance(getResources().getString(R.string.slide1_title)
+                ,getResources().getString(R.string.slide1_desc)
+                ,R.drawable.slide1
+                ,getResources().getColor(R.color.white)
+                ,getResources().getColor(R.color.colorPrimary)
+                ,getResources().getColor(R.color.colorPrimaryDark)));
+
+        addSlide(AppIntroFragment.newInstance(getResources().getString(R.string.slide2_title)
+                ,getResources().getString(R.string.slide2_desc)
+                ,R.drawable.slide2
+                ,getResources().getColor(R.color.white)
+                ,getResources().getColor(R.color.colorPrimary)
+                ,getResources().getColor(R.color.colorPrimaryDark)));
+
+        addSlide(AppIntroFragment.newInstance(getResources().getString(R.string.slide3_title)
+                ,getResources().getString(R.string.slide3_desc)
+                ,R.drawable.slide3
+                ,getResources().getColor(R.color.white)
+                ,getResources().getColor(R.color.colorPrimary)
+                ,getResources().getColor(R.color.colorPrimaryDark)));
+
+        setBarColor(getResources().getColor(R.color.colorAccent));
+        showSkipButton(false);
+    }
+
+    @Override
+    public void onSkipPressed(Fragment currentFragment) {
+        super.onSkipPressed(currentFragment);
+        finish();
+    }
+
+    @Override
+    public void onDonePressed(Fragment currentFragment) {
+        super.onDonePressed(currentFragment);
+        finish();
+    }
+
+}
